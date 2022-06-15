35319aaef84a1228f5a5ff40d53c2b8dd9a57fae
INnoVatioN-97 <koyoungil97@gmail.com>
2022-06-15 15:39:12 +0900

    기본적인 블록체인 모양새 구성

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { init, exit } from './customModules/myPackage';
+import crypto from 'crypto';
 
-init({ url: 'hi', debug: false });
+interface BlockShape {
+	hash: string;
+	prevHash: string;
+	height: number;
+	data: string;
+}
 
-exit(1);
+class Block implements BlockShape {
+	public hash: string;
+	constructor(public prevHash: string, public height: number, public data: string) {
+		this.hash = Block.calcalateHash(prevHash, height, data);
+	}
+
+	static calcalateHash(prevHash: string, height: number, data: string) {
+		const toHash: string = `${prevHash}${height}${data}`;
+		return crypto.createHash('sha256').update(toHash).digest('hex');
+	}
+}
